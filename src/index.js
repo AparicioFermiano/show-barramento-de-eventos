80f9f0a703505e811b0887e4285766b68b2f2a66
AparicioFermiano <aparicio.empresarial@gmail.com>
2021-05-17 13:20:56 -0300

    Atualização

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 const port = 10000
+app.use(express.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.post('/eventos', (req, res) => {
+    const eventos = req.body;
+    axios.post('http://localhost:2000/eventos', eventos);
+    axios.post('http://localhost:3000/eventos', eventos);
+    axios.post('http://localhost:5000/eventos', eventos);
+    res.status(200).send({msg: "ok"})
+})
+
+app.get("/eventos", (req, res) => {
+    res.send(eventos);
+});
+app.listen(port, () => console.log('Barramento, Porta 10000'))
